e2068c3cb55d7edcdc6104e6fdfa9cd0e8c65b89
karolzak <karol.zak@hotmail.com>
2019-03-13 12:07:25 +0100

    added base unet implementation

---
+++ keras_unet/models/unet.py
@@ -0,0 +1,72 @@
+from keras.models import Model
+from keras.layers import BatchNormalization, Conv2D, Conv2DTranspose, MaxPooling2D, Dropout, UpSampling2D, Input, concatenate
+
+def upsample_conv(filters, kernel_size, strides, padding):
+    return Conv2DTranspose(filters, kernel_size, strides=strides, padding=padding)
+
+def upsample_simple(filters, kernel_size, strides, padding):
+    return UpSampling2D(strides)
+
+def conv2d_block(
+    inputs, 
+    use_batch_norm=True, 
+    use_dropout=True, 
+    dropout=0.5, 
+    filters=16, 
+    kernel_size=(3,3), 
+    activation='relu', 
+    kernel_initializer='he_normal', 
+    padding='same'):
+    
+    c = Conv2D(filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer, padding=padding) (inputs)
+    if use_batch_norm:
+        c = BatchNormalization()(c)
+    if use_dropout:
+        c = Dropout(dropout)(c)
+    c = Conv2D(filters, kernel_size, activation=activation, kernel_initializer=kernel_initializer, padding=padding) (c)
+    if use_batch_norm:
+        c = BatchNormalization()(c)
+    return c
+
+def unet(
+    input_shape,
+    use_batch_norm=True, 
+    upsample_mode='deconv', # 'deconv' or 'simple' 
+    use_dropout=True, 
+    use_dropout_on_upsampling=False, 
+    dropout=0.3, 
+    dropout_change_per_layer=0.0,
+    filters=16,
+    num_layers=4,
+    output_activation='sigmoid'): # 'sigmoid' or 'softmax'
+    
+    if upsample_mode=='deconv':
+        upsample=upsample_conv
+    else:
+        upsample=upsample_simple
+
+    # Build U-Net model
+    inputs = Input(input_shape)
+    x = inputs   
+
+    down_layers = []
+    for l in range(num_layers):
+        x = conv2d_block(inputs=x, filters=filters, use_batch_norm=use_batch_norm, use_dropout=use_dropout, dropout=dropout)
+        down_layers.append(x)
+        x = MaxPooling2D((2, 2)) (x)
+        dropout += dropout_change_per_layer
+        filters = filters*2 # double the number of filters with each layer
+
+    x = conv2d_block(inputs=x, filters=filters, use_batch_norm=use_batch_norm, use_dropout=use_dropout, dropout=dropout)
+    use_dropout=use_dropout_on_upsampling
+    
+    for conv in reversed(down_layers):        
+        filters //= 2 # decreasing number of filters with each layer    
+        x = upsample(filters, (2, 2), strides=(2, 2), padding='same') (x)
+        x = concatenate([x, conv])
+        x = conv2d_block(inputs=x, filters=filters, use_batch_norm=use_batch_norm, use_dropout=use_dropout, dropout=dropout)
+    
+    outputs = Conv2D(1, (1, 1), activation=output_activation) (x)    
+    
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
