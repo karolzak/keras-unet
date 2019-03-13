7f46158874bff1e57a11cdf5dc946b347973a081
karolzak <karol.zak@hotmail.com>
2019-03-13 12:07:44 +0100

    added unet for satellite images imlementation

---
+++ keras_unet/models/unet_satellite.py
@@ -0,0 +1,99 @@
+# https://cdn-sv1.deepsense.ai/wp-content/uploads/2017/04/architecture_details.png
+# https://deepsense.ai/deep-learning-for-satellite-imagery-via-image-segmentation/
+
+from keras.models import Model
+from keras.layers import BatchNormalization, Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D, Input, concatenate
+
+def bn_conv_relu(input, filters, bachnorm_momentum, **conv2d_args):
+    x = BatchNormalization(momentum=bachnorm_momentum)(input)
+    x = Conv2D(filters, **conv2d_args)(x)
+    return x
+
+def bn_upconv_relu(input, filters, bachnorm_momentum, **conv2d_trans_args):
+    x = BatchNormalization(momentum=bachnorm_momentum)(input)
+    x = Conv2DTranspose(filters, **conv2d_trans_args)(x)
+    return x
+
+def unet_satellite(
+    input_shape,    
+    output_activation='sigmoid',
+    #use_batch_norm=True, 
+    #upsample_mode='DECONV', 
+    #use_dropout=True, 
+    #use_dropout_on_upsampling=False, 
+    #dropout=0.1, 
+    #dropout_change_per_layer=0.0,
+    #filters=16,
+    num_layers=4
+):
+    inputs = Input(input_shape)   
+    
+    filters = 64
+    upconv_filters = 96
+
+    kernel_size = (3,3)
+    activation = 'relu'
+    strides = (1,1)
+    padding = 'same'
+    kernel_initializer = 'he_normal'
+
+    conv2d_args = {
+        'kernel_size':kernel_size,
+        'activation':activation, 
+        'strides':strides,
+        'padding':padding,
+        'kernel_initializer':kernel_initializer
+        }
+
+    conv2d_trans_args = {
+        'kernel_size':kernel_size,
+        'activation':activation, 
+        'strides':(2,2),
+        'padding':padding,
+        'output_padding':(1,1)
+        }
+
+    bachnorm_momentum = 0.01
+
+    pool_size = (2,2)
+    pool_strides = (2,2)
+    pool_padding = 'valid'
+
+    maxpool2d_args = {
+        'pool_size':pool_size,
+        'strides':pool_strides,
+        'padding':pool_padding,
+        }
+    
+    x = Conv2D(filters, **conv2d_args)(inputs)
+    c1 = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)    
+    x = bn_conv_relu(c1, filters, bachnorm_momentum, **conv2d_args)
+    x = MaxPooling2D(**maxpool2d_args)(x)
+
+    down_layers = []
+
+    for l in range(num_layers):
+        x = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)
+        x = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)
+        down_layers.append(x)
+        x = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)
+        x = MaxPooling2D(**maxpool2d_args)(x)
+
+    x = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)
+    x = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)
+    x = bn_upconv_relu(x, filters, bachnorm_momentum, **conv2d_trans_args)
+
+    for conv in reversed(down_layers):        
+        x = concatenate([x, conv])  
+        x = bn_conv_relu(x, upconv_filters, bachnorm_momentum, **conv2d_args)
+        x = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)
+        x = bn_upconv_relu(x, filters, bachnorm_momentum, **conv2d_trans_args)
+
+    x = concatenate([x, c1])
+    x = bn_conv_relu(x, upconv_filters, bachnorm_momentum, **conv2d_args)
+    x = bn_conv_relu(x, filters, bachnorm_momentum, **conv2d_args)
+           
+    outputs = Conv2D(1, kernel_size=(1,1), strides=(1,1), activation=output_activation, padding='valid') (x)       
+    
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
