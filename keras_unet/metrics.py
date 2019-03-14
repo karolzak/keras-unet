bca6b830f21e33262f8d618c756dbb2b9135bac2
karolzak <karol.zak@hotmail.com>
2019-03-14 13:30:09 +0100

    added basic losses and metrics

---
+++ keras_unet/metrics.py
@@ -0,0 +1,31 @@
+from keras import backend as K
+import tensorflow as tf
+
+
+def iou(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
+
+
+def threshold_binarize(x, threshold=0.5):
+    ge = tf.greater_equal(x, tf.constant(threshold))
+    y = tf.where(ge, x=tf.ones_like(x), y=tf.zeros_like(x))
+    return y
+
+
+def iou_thresholded(y_true, y_pred, threshold=0.5, smooth=1.):
+    y_pred = threshold_binarize(y_pred, threshold)
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
+
+
+def dice_coef(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (
+                K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
