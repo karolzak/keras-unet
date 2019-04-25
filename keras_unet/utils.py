3285eeca85dd7ae0002af94773c5d23e43c3589d
karolzak <karol.zak@hotmail.com>
2019-04-25 05:05:46 +0200

    modified image plotting to work with different shapes

--- keras_unet/utils.py
+++ keras_unet/utils.py
@@ -74,11 +74,12 @@ def plot_segm_history(history, metrics=['iou', 'val_iou'], losses=['loss', 'val_
     plt.show()
 
 
-def mask_to_red(mask, img_size=1024):
+def mask_to_red(mask):
     '''
     Converts binary segmentation mask from white to red color.
     Also adds alpha channel to make black background transparent.
     '''
+    img_size = mask.shape[0]
     c1 = mask.reshape(img_size,img_size)
     c2 = np.zeros((img_size,img_size))
     c3 = np.zeros((img_size,img_size))
@@ -86,11 +87,12 @@ def mask_to_red(mask, img_size=1024):
     return np.stack((c1, c2, c3, c4), axis=-1)
 
 
-def mask_to_rgba(mask, img_size=1024, color='red'):
+def mask_to_rgba(mask, color='red'):
     '''
     Converts binary segmentation mask from white to red color.
     Also adds alpha channel to make black background transparent.
     '''
+    img_size = mask.shape[0]
     zeros = np.zeros((img_size,img_size))
     ones = mask.reshape(img_size,img_size)
     if color == 'red':
@@ -111,8 +113,7 @@ def plot_imgs(org_imgs,
               mask_imgs, 
               pred_imgs=None, 
               nm_img_to_plot=10, 
-              figsize=4, 
-              img_size=1024, 
+              figsize=4,
               alpha=0.5
              ):
     '''
@@ -120,12 +121,19 @@ def plot_imgs(org_imgs,
     Last column is always an overlay of ground truth or prediction
     depending on what was provided as arguments.
     '''
-    #nm_img_to_plot = org_imgs.shape[0]
+    if nm_img_to_plot > org_imgs.shape[0]:
+        nm_img_to_plot = org_imgs.shape[0]
     im_id = 0
+    org_imgs_size = org_imgs.shape[1]
+
+    org_imgs = reshape_arr(org_imgs)
+    mask_imgs = reshape_arr(mask_imgs)
     if  not (pred_imgs is None):
         cols = 4
+        pred_imgs = reshape_arr(pred_imgs)
     else:
         cols = 3
+
         
     fig, axes = plt.subplots(nm_img_to_plot, cols, figsize=(cols*figsize, nm_img_to_plot*figsize))
     axes[0, 0].set_title("original", fontsize=15) 
@@ -136,20 +144,46 @@ def plot_imgs(org_imgs,
     else:
         axes[0, 2].set_title("overlay", fontsize=15) 
     for m in range(0, nm_img_to_plot):
-        axes[m, 0].imshow(org_imgs[im_id])
+        axes[m, 0].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
         axes[m, 0].set_axis_off()
-        axes[m, 1].imshow(mask_imgs[im_id].reshape((img_size,img_size)), cmap='gray')
+        axes[m, 1].imshow(mask_imgs[im_id], cmap=get_cmap(mask_imgs))
         axes[m, 1].set_axis_off()        
         if not (pred_imgs is None):
-            axes[m, 2].imshow(pred_imgs[im_id].reshape((img_size,img_size)), cmap='gray')
+            axes[m, 2].imshow(pred_imgs[im_id], cmap=get_cmap(pred_imgs))
             axes[m, 2].set_axis_off()
-            axes[m, 3].imshow(org_imgs[im_id])
-            axes[m, 3].imshow(mask_to_red(pred_imgs[im_id].reshape((img_size,img_size)), img_size=img_size), alpha=alpha)
+            axes[m, 3].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
+            axes[m, 3].imshow(mask_to_red(zero_pad_mask(pred_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(pred_imgs), alpha=alpha)
             axes[m, 3].set_axis_off()
         else:
-            axes[m, 2].imshow(org_imgs[im_id])
-            axes[m, 2].imshow(mask_to_red(mask_imgs[im_id].reshape((img_size,img_size)), img_size=img_size), alpha=alpha)
+            axes[m, 2].imshow(org_imgs[im_id], cmap=get_cmap(org_imgs))
+            axes[m, 2].imshow(mask_to_red(zero_pad_mask(mask_imgs[im_id], desired_size=org_imgs_size)), cmap=get_cmap(mask_imgs), alpha=alpha)
             axes[m, 2].set_axis_off()
         im_id += 1
 
     plt.show()
+
+
+def zero_pad_mask(mask, desired_size):
+    pad = (desired_size - mask.shape[0]) // 2
+    padded_mask = np.pad(mask, pad, mode="constant")
+    return padded_mask
+
+
+def reshape_arr(arr):
+    if arr.ndim == 3:
+        return arr
+    elif arr.ndim == 4:
+        if arr.shape[3] == 3:
+            return arr
+        elif arr.shape[3] == 1:
+            return arr.reshape(arr.shape[0], arr.shape[1], arr.shape[2])
+
+
+def get_cmap(arr):
+    if arr.ndim == 3:
+        return 'gray'
+    elif arr.ndim == 4:
+        if arr.shape[3] == 3:
+            return 'rgb'
+        elif arr.shape[3] == 1:
+            return 'gray'
