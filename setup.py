4787bc9b7a099185dee413b8095b4a8a1a364fb6
karolzak <karol.zak@hotmail.com>
2019-05-31 16:21:40 +0000

    minor changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='keras-unet',
-      version='0.0.4',
+      version='0.0.5',
       description='U-Net model for semantic segmentation implementation in Keras + useful utility functions for semantic segmentation tasks',
       url='http://github.com/karolzak/keras-unet',
       author='Karol Zak',
